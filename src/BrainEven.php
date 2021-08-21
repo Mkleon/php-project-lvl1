f3d01dd8b20d3fd247962f6b9c7b2c93aa195727
Kirill <kirillo81@mail.ru>
2021-08-21 18:57:05 +0300

    correct

---
+++ src/BrainEven.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Brain\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+function even()
+{
+    $gamesCount = 3;
+
+    line('Welcome to the Brain Games!');
+
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+
+    for ($i = 1; $i <= $gamesCount; $i++) {
+        $number = rand(1, 99);
+        $correctAnswer = $number % 2 === 0 ? 'yes' : 'no';
+
+        line("Question: %s", $number);
+
+        $answer = prompt("Your answer");
+
+        if ($correctAnswer === $answer) {
+            line("Correct!");
+            continue;
+        }
+
+        line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $answer === 'no' ? 'yes' : 'no');
+        line("Let's try again, %name!", $name);
+        return;
+    }
+
+    line("Congratulations, %s!", $name);
+}
